46636bab19f28f8ead96c55ad779476813e4f80e
MrPotato123540 <demircanberkin@outlook.com>
2025-12-15 03:43:32 +0300

    Logger sinifi Sİngleton pattern ile yazildi (LLR-LOG-01...04)

--- modules/logger/include/Logger.h
+++ modules/logger/include/Logger.h
@@ -1,28 +1,63 @@
+//Dosya yanlışlıkla birden fazla kez include edilmesin diye kullanılır
+//define ile tanımlandıktan sonra endif kadar olan kısmı atlar
 #ifndef LOGGER_H
 #define LOGGER_H
 
 #include <string>
+#include <fstream>
 
+//LLR-LOG-02 e göre Logger Singleton kullanmalı
 class Logger {
 public:
-    enum Level {
-        INFO,
-        WARNING,
-        ERROR
-    };
+	//log dosyasına seviyeler vereceğiz
+	enum Level {
+		BILGI,
+		UYARI,
+		HATA
+	};
 
-    static Logger& instance();
+	//Singleton erişim metodu
+	//nesne oluşturulmadan çağırmak için static seçildi
+	static Logger* getInstance();
 
-    void log(Level level, const std::string& message);
+	//dışarıdan gelen veriyi alarak o anki saati ve tarihi hesaplayacak
+	//LLR-LOG-03 de belirtilen zaman damgalı log mesajı
+	//parametreleri seviye olarak alıyor
+	void log(Level level, const std::string& mesaj);
 
-    void info(const std::string& message);
-    void warning(const std::string& message);
-    void error(const std::string& message);
+	//leveller için fonksiyonlar
+	//& ile referans geçip optimizasyon sağlayıp tekrar tekrar hafızaya 
+	//kopyalamayacağız
+	//const sebebi de sadece okunsun değişmesin diye
+	void bilgi(const std::string& mesaj);
+	void uyari(const std::string& mesaj);
+	void hata(const std::string& mesaj);
+
+	//dosyayı kapama için
+	//LLR-LOG-04 de belirtilen dosyanın güvenli kapanması
+	void logKapa();
 
 private:
-    Logger();
-    Logger(const Logger&);
-    Logger& operator=(const Logger&);
+	//Singleton için constructor private yapılır
+	Logger();
+
+	//bu kısım Sngleton u bozmamak için eklendi
+	//logger pointer ile kopyalanabileceği için sıkıntı olabilir
+	
+	//bu kısım L2(L1) gibi işlemleri yasaklar
+	Logger(const Logger&);
+
+	//bu kısım L2 = L1 yasaklar
+	Logger& operator=(const Logger&);
+
+	//tekil instance tutucu
+	//static ile değişkenin üretilen değişkenden bağımszı sınıfa özgü olması
+	//sağlanacak yani program boyu bir tane instance pointer olacak
+	static Logger* instance; 
+	
+       	// Dosya yazma nesnesi REQ4 için lazım
+	std::ofstream logDosya; 
+	std::string dosyaAdi;
 };
 
 #endif
--- modules/logger/src/Logger.cpp
+++ modules/logger/src/Logger.cpp
@@ -1,36 +1,81 @@
 #include "Logger.h"
 #include <iostream>
+#include <ctime>
+#include <string>
+#include <cstring>
 
-Logger::Logger() {}
+//static instance başlatma
+Logger* Logger::instance = 0;
 
-Logger& Logger::instance() {
-    static Logger instance;
-    return instance;
+//constructor
+Logger::Logger() {
+	dosyaAdi = "log.txt";
+
+	//LLR-LOG-01 .log veya .txt uzantılı dosya oluşturma
+	//std::ios::app ve out (append ve yazma modları) ile dosya ekleme modunda açılır
+	// ve kayıtlar silinmez
+	logDosya.open(dosyaAdi.c_str(), std::ios::out | std::ios::app);
+
+	if (logDosya.is_open()) {
+		log(BILGI, "Log dosyasi baslatildi...");
+	} else {
+		std::cerr << "Hata: Log dosyasi acilamadi" << std::endl;
+	}
 }
 
-void Logger::log(Level level, const std::string& message) {
-    switch (level) {
-        case INFO:
-            std::cout << "[INFO] ";
-            break;
-        case WARNING:
-            std::cout << "[WARNING] ";
-            break;
-        case ERROR:
-            std::cout << "[ERROR] ";
-            break;
-    }
-    std::cout << message << std::endl;
+Logger* Logger::getInstance() {
+	//LLR-LOG-02 için instance yoksa oluştur varsa döndür
+	if (instance == 0) {
+		instance = new Logger();
+	}
+	return instance;
 }
 
-void Logger::info(const std::string& message) {
-    log(INFO, message);
+void Logger::log(Level level, const std::string &message) {
+	if (logDosya.is_open()) {
+		//zaman damgası oluşturulacak
+		time_t simdi = time(0);
+		//saniye vb okunabilir string e dönüştürülecek
+		//örn: "Sun Dec 14 18:00:00 2025\n"
+		char* zam = ctime(&simdi);
+
+		//ctime sonunda \n olacak yazım için o düzeltiliyor
+		if (zam && std::strlen(zam) > 0) {
+			if (zam[std::strlen(zam) - 1] == '\n') {
+			    zam[std::strlen(zam) - 1] = '\0';
+			}
+		}
+
+		std::string levelEtiketi;
+		switch (level) {
+			case BILGI: levelEtiketi = "[BILGI]"; break;
+			case UYARI: levelEtiketi = "[UYARI]"; break;
+			case HATA: levelEtiketi = "[HATA]"; break;
+			default:    levelEtiketi = "[BILINMIYOR]"; break;
+		}
+
+		logDosya << "[" << zam << "] " << levelEtiketi << message << std::endl;
+		//bufferdaki veriyi direk hedefe yazar
+		logDosya.flush();
+	}
 }
 
-void Logger::warning(const std::string& message) {
-    log(WARNING, message);
+void Logger::bilgi(const std::string &mesaj) {
+	log(BILGI, mesaj);
 }
 
-void Logger::error(const std::string& message) {
-    log(ERROR, message);
+void Logger::uyari(const std::string &mesaj) {
+	log(UYARI, mesaj);
+}
+
+void Logger::hata(const std::string &mesaj) {
+	log(HATA, mesaj);
+}
+
+
+void Logger::logKapa() {
+	if (logDosya.is_open()) {
+		log(BILGI, "Log dosyasi kapatiliyor");
+		logDosya.close();
+	}
 }
