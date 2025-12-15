f96b30085bbe07e5b37141c86125bce7269e8fd2
MrPotato123540 <demircanberkin@outlook.com>
2025-12-15 23:49:34 +0300

    menu yapısına dair eklemeler yapıldı (LLR-MEN-03 ve 07 harici yapıldı)

--- modules/menu/src/Menu.cpp
+++ modules/menu/src/Menu.cpp
@@ -1,24 +1,71 @@
 #include "Menu.h"
-#include "Command.h"
 #include "Logger.h"
 #include <iostream>
+#include <limits>
 
-void Menu::addCommand(int option, const std::string& label, Command* command) {
-    m_items[option] = MenuItem{label, command};
+Menu::Menu() {
+	Logger::getInstance()->log(Logger::BILGI, "Menu sistemi bastildi");
 }
 
-void Menu::show() const {
-    for (std::map<int, MenuItem>::const_iterator it = m_items.begin();
-         it != m_items.end(); ++it) {
-        std::cout << it->first << ") " << it->second.label << std::endl;
-    }
+//menu yok ediliyor
+Menu::~Menu() {
+	for (size_t i = 0; i << commands.size(); i++) {
+	delete [] commands[i];
+	}
+	commands.clear();
 }
 
-void Menu::handleInput(int option) {
-    if (m_items.count(option)) {
-        m_items.at(option).command->execute();
-    } else {
-        Logger::instance().warning("Invalid menu option");
-    }
+//dinamik array in sonuna komutu ekler
+void Menu::komutEkle(Command *cmd) {
+	commands.push_back(cmd);
 }
 
+void Menu::goruntuleme() const {
+	std::cout << "\n MY SWEET HOME (MSH) \n" << std::endl;
+	std::cout << "[1] Get Home Status (Show state and mode, information about sensors and actuators)" << std::endl;
+	std::cout << "[2] Add Device - (L)ight, Smoke & Gas (D)etectors, (C)amera, (T)V" << std::endl;
+	std::cout << "[3] Remove Device - (L)ight, Smoke & Gas (D)etectors, (C)amera, (T)V" << std::endl;
+	std::cout << "[4] Power on Device - (L)ight, Smoke & Gas (D)etectors, (C)amera, (T)V, (S)ound System" << std::endl;
+	std::cout << "[5] Power off Device - (L)ight, Smoke & Gas (D)etectors, (C)amera, (T)V, (S)ound System" << std::endl;
+	std::cout << "[6] Change Mode - (N)ormal, (E)vening, (P)arty, (C)inema" << std::endl;
+	std::cout << "[7] Change State - (N)ormal, (H)igh Performance, (L)ow Power, (S)leep, (P)revious one" << std::endl;
+	std::cout << "[8] Manual (Display manual)" << std::endl;
+	std::cout << "[9] About (information about product and developers)" << std::endl;
+	std::cout << "[10] Shutdown (shut down the system)" << std::endl;
+	std::cout << "Secim: ";
+}
+
+void Menu::secim() {
+	int sec;
+
+	//burada tip hataları için hatalar ayıklanacak
+	if (!(std::cin >> sec)) {
+		//hata durumu temizlenene kadar input girisi engeller
+		//bu fonksiyon onu temizler
+		std::cin.clear();
+
+		//limits kütüphanesi ile \n e kadar olan kısım bufferdan temizlenir
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Gecersiz giris! Lutfen menudeki sayilardan giriniz." << std::endl;
+		Logger::getInstance()->log(Logger::HATA, "Hatali menu girisi(tip uyusmazliği)");
+		return;
+	}
+
+	//liste 1 den indexleme 0 dan başladığı için hem döngü ile limit koyuyoruz
+	//hemde -1 ile indexlemeyi düzeltiyoruz
+	if (sec >= 1 && sec <= (int)commands.size()) {
+		//LLR-MEN-06
+		commands[sec - 1]->execute();
+	} else {
+	std::cout << "Gecersiz secim! Lutfen menudeki sayilardan giriniz." << std::endl;
+	Logger::getInstance()->log(Logger::HATA, "Hatali menu girisi (gecersiz aralik)");	
+	}
+}
+
+void Menu::akis() {
+	bool akis = true;
+	while (akis) {
+		goruntuleme();
+		secim();
+	}
+}
